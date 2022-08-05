a68105946bcd5fbb776c939966d51fd5ad00400f
Tanvirmehedi <tjmehedi201@gmail.com>
2022-08-06 01:51:18 +0600

    adding middleware

--- app/app.js
+++ app/app.js
@@ -1,5 +1,12 @@
+//require dotenv and give the path
+require("dotenv").config("../.env");
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
+
+// Initialization Global Middleware
+app.use([morgan("dev"), cors(), express.json()]);
 
 app.get("/health", (_req, res, _next) => {
   res.status(200).json({ message: "Success" });
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const http = require("http");
 
-const app = require("./app/app");
+require("dotenv").config();
 
+const app = require("./app/app");
+//create server
 const server = http.createServer(app);
 
-server.listen(8000, () => {
-  console.log("Server IS running");
+//graph PORT on .env
+const PORT = process.env.PORT || 8000;
+
+// listening to server according to port
+server.listen(PORT, () => {
+  console.log("Server IS running", PORT);
 });
